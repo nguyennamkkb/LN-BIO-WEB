37f899425618d18dd150705b9b0fba1286186091
Your Name <you@example.com>
2023-08-12 11:00:21 +0700

    nam

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,58 +2,103 @@
   <div style="display: inline-block; width: 90%" class="main">
     <div class="header">
       <div class="demo-image">
-        <el-image style="width: 150px; height: 150px; border-radius: 10px" :src="url" :fit="fit" />
+        <el-image
+          style="width: 150px; height: 150px; border-radius: 10px"
+          :src="url"
+          :fit="fit"
+        />
       </div>
       <div class="lb-bio">
         <p>Nguyễn Lương Nam<br />Lap trinh vien</p>
       </div>
     </div>
     <div class="Mid">
-      <a style="color: navajowhite; font-weight: 700; text-shadow: 2px 2px 5px rgb(2, 1, 1);" @click="centerDialogVisible = true"> Chia sẻ qua QR</a>
+      <a
+        style="
+          color: navajowhite;
+          font-weight: 700;
+          text-shadow: 2px 2px 5px rgb(2, 1, 1);
+        "
+        @click="centerDialogVisible = true"
+      >
+        Chia sẻ qua QR</a
+      >
+      
       <div class="btn-share">
-        <img src="/img/phone.png" class="img-btn-share"
-          width="35" height="35">
-          <el-button type="success" :icon="Download" circle  style="position: absolute;right: 10px;"/>
-        <p class="lb-btn-share"><a href="tel:0454545454">0454545454</a></p>
+        <img
+          src="/img/phone.png"
+          class="img-btn-share"
+          width="35"
+          height="35"
+        />
+        <el-button
+          type="success"
+          :icon="Download"
+          circle
+          style="position: absolute; right: 10px"
+        />
+        <p class="lb-btn-share"><a href="tel:0454545454">ádasdasdas</a></p>
       </div>
       <div class="btn-share">
-        
-        <img src="/img/facebook.png" class="img-btn-share"
-          width="35" height="35">
-        <p class="lb-btn-share"><a href="https://www.facebook.com/nguyennamkkb" target="_blank"> Nguyễn Lương Nam</a></p>
+        <img
+          src="/img/facebook.png"
+          class="img-btn-share"
+          width="35"
+          height="35"
+        />
+        <p class="lb-btn-share">
+          <a href="https://www.facebook.com/nguyennamkkb" target="_blank">
+            NguyễnLương NamNguyễnLương NamNguyễnLương Nam</a
+          >
+        </p>
       </div>
-     
+      <div class="btn-share" v-for="item in list" :key="item.id">
+        <img
+          v-bind:src= item.urlIcon
+          class="img-btn-share"
+          width="35"
+          height="35"
+        />
+        <p class="lb-btn-share">
+          <a v-bind:href=item.url target="_blank">
+            {{ item.name }}</a
+          >
+        </p>
+      </div>
     </div>
   </div>
 
-
   <!-- dialog -->
-  <el-dialog v-model="centerDialogVisible" title="Quét để chia sẻ" height="auto" center>
-   <div style="display: flex; align-items: center;  justify-content: center;">
-    <img src="/img/qr.png"
-          width="200" height="200">
-   </div>
+  <el-dialog
+    v-model="centerDialogVisible"
+    title="Quét để xem thông tin"
+    height="auto"
+    width="auto"
+    center
+  >
+    <div style="display: flex; align-items: center; justify-content: center">
+      <img src="/img/qr.png" width="200" height="200" />
+    </div>
   </el-dialog>
 </template>
 
 <style>
-
-#app{
+#app {
   background-image: url("/img/pxfuel.jpg");
   background-size: cover;
   height: 100vh;
 }
 
 /* .main { */
-   /* border-style: ridge;
+/* border-style: ridge;
    border-color: rgb(245, 164, 106); */
-   /* border-width: 0.5px; */
+/* border-width: 0.5px; */
 /* } */
 .header {
   width: 100%;
   height: auto;
   display: inline-block;
-  background-color:rgba(240, 240, 240,0.2);
+  background-color: rgba(240, 240, 240, 0.2);
   border-bottom-left-radius: 25px;
   border-bottom-right-radius: 25px;
   color: red;
@@ -80,7 +125,6 @@
   padding-top: 10px;
   /* display: inline-block; */
   /* background-color: aquamarine; */
-
 }
 
 .btn-share {
@@ -122,21 +166,43 @@
   width: auto;
   color: blue;
 }
-a{
-  text-decoration:none
+.lb-btn-share a {
+  text-decoration: none;
+  margin-right: 5px;
+  width: auto;
+  word-wrap: break-word;
 }
 </style>
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { Edit, Download, Share } from "@element-plus/icons-vue";
 
-import {
-  Edit,
-  Download,
-  Share
-} from '@element-plus/icons-vue'
-
-const centerDialogVisible = ref(false)
+const centerDialogVisible = ref(false);
 const fit = "cover";
 const url =
   "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg";
+</script>
+
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      list: [
+        {
+          id:1,
+          urlIcon: "/img/phone.png",
+          name: "0358 73 73 73",
+          url: "tel:0454545454",
+        },
+        {
+          id:2,
+          urlIcon: "/img/facebook.png",
+          name: "Nguyễn Lương Nam",
+          url: "https://www.facebook.com/nguyennamkkb",
+        },
+      ],
+    };
+  },
+};
 </script>
